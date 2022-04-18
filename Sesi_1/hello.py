9d8e8eeef4c25f9dd6e4f059c518892be65befaa
Farhan Eka Fajri <farhanekaf@gmail.com>
2022-04-18 10:20:02 +0700

    assignment1 python di sesi2

---
+++ Sesi_1/hello.py
@@ -0,0 +1,37 @@
+print('hello world')
+
+#integer
+
+print(123123123123123123123123123123123123123123123123 + 1)
+
+# Python interprets a sequence of decimal digits without any prefix to be a decimal number:
+print(type(123123123123123123123123123123123123123123123123 + 1))
+
+print(4.2)
+print(type(4.2))
+
+print(4.)
+
+print(.2)
+
+print(.4e7)
+
+print(4.2e-4)
+
+#String
+print("Hacktiv8")
+print(type("Hacktiv8"))
+
+#empty string 
+print('')
+
+print("This string contains a single quote (') character.")
+
+print('This string contains a double quote (") character.')
+
+#Boolean
+print(type(True))
+
+print(type(False))
+
+
