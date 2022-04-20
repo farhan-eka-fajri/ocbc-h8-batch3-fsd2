176b95e1ea77a86051cffe27b33a6e40650508dd
Farhan Eka Fajri <farhanekaf@gmail.com>
2022-04-20 16:14:00 +0700

    test version 2

--- Sesi_5/kelas.py
+++ Sesi_5/kelas.py
@@ -20,9 +20,37 @@ buddy = Dog("Buddy", 9, "Dachshund")
 jack = Dog("Jack", 3, "Bulldog")
 jim = Dog("Jim", 5, "Bulldog")
 
-buddy.speak("Yap")
+print(buddy.speak("Yap"))
 jim.speak("Woof")
 
-print(buddy.speak)
+description_of_buddy = buddy.description()
+print(description_of_buddy)
 
+speak_of_buddy = buddy.speak("Yap Yap")
+
+#inheritance
+
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+    # Instance method
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+
+    # Another instance method
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+class corgi(Dog): 
+    #override speak method
+    def speak(self,sound = "roof roof", activities="eating around"):
+        return f"{self.name} says {sound}, and love to {activities}"
+
+auri = corgi("auri",9,"corgi")
+print(auri.speak())
 
